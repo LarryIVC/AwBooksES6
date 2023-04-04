5cc45483e5e9f39e1cb73312ca7d8ff1d0f0d1f0
Larry Villegas <larry_villegas@hotmail.com>
2023-04-04 12:09:23 -0400

    Update logica.js

--- modules/logica.js
+++ modules/logica.js
@@ -1,35 +1,5 @@
-import { DateTime } from "../node_modules/luxon/build/es6/luxon.js";
-import { BookList } from "./BookList.js";
-
-export function logica() {
-
-  const bookListElement = document.getElementById('book-list');
-  const addBookFormElement = document.getElementById('add-book-form');
-  const titleInputElement = document.getElementById('title-input');
-  const authorInput = document.getElementById('author-input');
-
-  const bookList = new BookList(bookListElement, addBookFormElement, titleInputElement, authorInput);
-  bookList.loadBooks();
-
-  hideSectionsOnClick();
-
-  const options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    hour12: true,
-  };
-  DateTime.local();
-  // const now = new Date();
-  const now = DateTime.now();
-  const currentDate = now.toLocaleString(DateTime.DATETIME_MED) ; /* .toLocaleDateString('en-US', options); */
-  const xDateTime = document.getElementById('date-time');
-  xDateTime.innerHTML = currentDate;
-
-}
+import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
+import { BookList } from './BookList.js';
 
 function hideSectionsOnClick() {
   const listSection = document.getElementById('all-books');
@@ -57,4 +27,38 @@ function hideSectionsOnClick() {
     addNewSection.style.display = 'none';
     contactSection.style.display = 'flex';
   });
+}
+
+export function logica() {
+  const bookListElement = document.getElementById('book-list');
+  const addBookFormElement = document.getElementById('add-book-form');
+  const titleInputElement = document.getElementById('title-input');
+  const authorInput = document.getElementById('author-input');
+
+  const bookList = new BookList(
+    bookListElement,
+    addBookFormElement,
+    titleInputElement,
+    authorInput,
+  );
+
+  bookList.loadBooks();
+
+  hideSectionsOnClick();
+
+  // const options = {
+  //   year: 'numeric',
+  //   month: 'long',
+  //   day: 'numeric',
+  //   hour: 'numeric',
+  //   minute: 'numeric',
+  //   second: 'numeric',
+  //   hour12: true,
+  // };
+  DateTime.local();
+  // const now = new Date();
+  const now = DateTime.now();
+  const currentDate = now.toLocaleString(DateTime.DATETIME_MED);
+  const xDateTime = document.getElementById('date-time');
+  xDateTime.innerHTML = currentDate;
 }
