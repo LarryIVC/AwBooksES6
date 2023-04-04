c0c925eb563d5e04105cf99ee69cf5a58cd6e736
Larry Villegas <larry_villegas@hotmail.com>
2023-04-04 12:54:17 -0400

    Update Page.js

--- modules/Page.js
+++ modules/Page.js
@@ -1,12 +1,14 @@
-import { Footer } from './Footer.js';
-import { Header } from './Header.js';
-import { MainCont } from './MainCont.js';
-import { logica } from './logica.js';
+import Footer from './Footer.js';
+import Header from './Header.js';
+import MainCont from './MainCont.js';
+import logica from './logica.js';
 
-export function Page() {
+const Page = () => {
   const myPage = document.getElementById('page');
   myPage.appendChild(Header());
   myPage.appendChild(MainCont());
   myPage.appendChild(Footer());
   logica();
-}
+};
+
+export default Page;
