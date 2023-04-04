6ddf13b179b59e224378983bb81f1f6f77bd6575
Larry Villegas <larry_villegas@hotmail.com>
2023-04-04 13:00:53 -0400

    Update Footer.js

--- modules/Footer.js
+++ modules/Footer.js
@@ -1,8 +1,8 @@
- const Footer = () => {
+const Footer = () => {
   const doc = document;
   const myFooter = doc.createElement('footer');
   myFooter.innerHTML = '<p>Copyright &copy; 2023 Awesome Books</p>';
   return myFooter;
-}
+};
 
 export default Footer;
