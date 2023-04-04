6bfd03e3eeb31080191d952e832f2f43ed60bdf2
Larry Villegas <larry_villegas@hotmail.com>
2023-04-04 10:20:31 -0400

    Create MainCont.js

---
+++ modules/MainCont.js
@@ -0,0 +1,28 @@
+export function MainCont() {
+  const doc = document,
+  myMain = doc.createElement('main');
+  myMain.innerHTML = `<section id="all-books">
+    <h1>All Awesome Books</h1>
+    <ul id="book-list"><p>No Books yet, please add ...</p></ul>
+    </section>
+    <section id="add-book">
+      <form id="add-book-form">
+        <h2>Add a new book</h2>
+        <label for="title-input"></label>
+        <input type="text" id="title-input" name="title" placeholder="Title" required>
+        <label for="author-input"></label>
+        <input type="text" id="author-input" name="author" placeholder="Author" required>
+        <button type="submit" id="add-button">Add</button>
+      </form>
+    </section>
+    <section id="contact-us">
+      <h1>Contact Information</h1>
+      <p>If you have any questions or you just want to say "Hello", you can reach out to us!</p>
+      <ul>
+        <li>Our e-mail: <a href="mailto:naanahmed.na@gmail.com" target="_blank">naanahmed.na@gmail.com</a></li>
+        <li>Our e-mail: <a href="mailto:Larry.villegas@gmail.com" target="_blank">Larry.villegas@gmail.com</a></li>
+        <li>Our e-mail: <a href="mailto:abiola.arobieke@gmail.com" target="_blank">abiola.arobieke@gmail.com</a></li>
+      </ul>
+    </section>`;
+  return myMain; 
+}
