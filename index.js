f35507a64d7c467560ef6ea2000fc4c082744cf0
Larry Villegas <larry_villegas@hotmail.com>
2023-04-04 12:09:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Page } from "./modules/Page.js";
+import { Page } from './modules/Page.js';
 
-const d = document;
-d.addEventListener('DOMContentLoaded',Page);
+const doc = document;
+doc.addEventListener('DOMContentLoaded', Page());
