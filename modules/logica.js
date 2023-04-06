3c04d1fffebe1444af8ffcf9e8631f81832ff0a9
Larry Villegas <larry_villegas@hotmail.com>
2023-04-05 23:04:21 -0400

    Update logica.js

--- modules/logica.js
+++ modules/logica.js
@@ -1,4 +1,4 @@
-import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
+import { DateTime } from './luxon.js';
 import BookList from './BookList.js';
 
 function hideSectionsOnClick() {
